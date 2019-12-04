8781a68574c688031ebcad0d0499c314dfd0d3d5
luisfynn <luisfynn@bodyfriend.co.kr>
2019-12-04 21:18:05 +0900

    update massage test fw

---
+++ MassageMapTest/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace MassageMapTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.comboBox7 = new System.Windows.Forms.ComboBox();
+            this.comboBox8 = new System.Windows.Forms.ComboBox();
+            this.comboBox9 = new System.Windows.Forms.ComboBox();
+            this.comboBox10 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.Font = new System.Drawing.Font("굴림", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(440, 94);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 19);
+            this.comboBox4.TabIndex = 5;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.Font = new System.Drawing.Font("굴림", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(567, 95);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 19);
+            this.comboBox5.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("굴림", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(26, 94);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 19);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("굴림", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(155, 95);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(148, 19);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("굴림", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(313, 95);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 19);
+            this.comboBox3.TabIndex = 2;
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.Font = new System.Drawing.Font("굴림", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(695, 94);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(121, 19);
+            this.comboBox6.TabIndex = 3;
+            // 
+            // comboBox7
+            // 
+            this.comboBox7.Font = new System.Drawing.Font("굴림", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.comboBox7.FormattingEnabled = true;
+            this.comboBox7.Location = new System.Drawing.Point(822, 95);
+            this.comboBox7.Name = "comboBox7";
+            this.comboBox7.Size = new System.Drawing.Size(121, 19);
+            this.comboBox7.TabIndex = 9;
+            // 
+            // comboBox8
+            // 
+            this.comboBox8.Font = new System.Drawing.Font("굴림", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.comboBox8.FormattingEnabled = true;
+            this.comboBox8.Location = new System.Drawing.Point(949, 95);
+            this.comboBox8.Name = "comboBox8";
+            this.comboBox8.Size = new System.Drawing.Size(121, 19);
+            this.comboBox8.TabIndex = 8;
+            // 
+            // comboBox9
+            // 
+            this.comboBox9.Font = new System.Drawing.Font("굴림", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.comboBox9.FormattingEnabled = true;
+            this.comboBox9.Location = new System.Drawing.Point(1076, 94);
+            this.comboBox9.Name = "comboBox9";
+            this.comboBox9.Size = new System.Drawing.Size(121, 19);
+            this.comboBox9.TabIndex = 7;
+            // 
+            // comboBox10
+            // 
+            this.comboBox10.Font = new System.Drawing.Font("굴림", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.comboBox10.FormattingEnabled = true;
+            this.comboBox10.Location = new System.Drawing.Point(1203, 94);
+            this.comboBox10.Name = "comboBox10";
+            this.comboBox10.Size = new System.Drawing.Size(121, 19);
+            this.comboBox10.TabIndex = 6;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(26, 156);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1265, 745);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 12);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Type";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(155, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "REMOVE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1383, 897);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox7);
+            this.Controls.Add(this.comboBox8);
+            this.Controls.Add(this.comboBox9);
+            this.Controls.Add(this.comboBox10);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MassageMap test program V0.1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.ComboBox comboBox7;
+        private System.Windows.Forms.ComboBox comboBox8;
+        private System.Windows.Forms.ComboBox comboBox9;
+        private System.Windows.Forms.ComboBox comboBox10;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MassageMapTest/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MassageMapTest
+{
+    public struct MassageMap
+    {
+        public string massageType;
+        public string walkHoldTime;
+        public string walkSpeed;
+        public string width;
+        //public string tapKneadDuty;  //tap<<4 | knead
+        public string tDuty;  //tap
+        public string kDuty;  //knead
+        public string xdHoldTime;
+        public string xdDuty;
+        public string xdRepeat;
+        public string xdAdj;
+        public string interwork;
+
+        public MassageMap(string _type, string _whold, string _wduty, string _width, string _tduty, string _kduty, string _xhold, string _xdDuty, string _repeat, string _adj, string _inwork)
+        {
+            massageType = _type;
+            walkHoldTime = _whold;
+            walkSpeed = _wduty;
+            width = _width;
+            //tapKneadDuty = _duty;
+            tDuty = _tduty;
+            kDuty = _kduty;
+            xdHoldTime = _xhold;
+            xdDuty = _xdDuty;
+            xdRepeat = _repeat;
+            xdAdj = _adj;
+            interwork = _inwork;
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        public List<MassageMap> massageList;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            massageList = new List<MassageMap>();
+
+#if false
+            string[] massageType = { "MASSAGE_TYPE_KNEAD", "MASSAGE_TYPE_KNEAD_R", "MASSAGE_TYPE_TAP", "MASSAGE_TYPE_MIX", "MASSAGE_TYPE_PRESS"
+                                    ,"MASSAGE_TYPE_SOFT_TAP", "MASSAGE_TYPE_SWING", "MASSAGE_TYPE_SWING_TAP", "MASSAGE_TYPE_SWING_SOFT_TAP", "MASSAGE_TYPE_KNEAD_SOFT_TAP"  };
+#endif
+            string[] comboMassageType = { "KNEAD", "KNEAD_R", "TAP", "MIX", "PRESS"
+                                    ,"SOFT_TAP", "SWING", "SWING_TAP", "SWING_SOFT_TAP", "KNEAD_SOFT_TAP"  };
+            comboBox1.Items.AddRange(comboMassageType);
+
+#if false
+            string[] comboWalkLocation = { "WALK_LOCATE_PARK", "WALK_LOCATE_TOP", "WALK_LOCATE_HEAD_BACK", "WALK_LOCATE_NECK_POSITION", "WALK_LOCATE_NECK_MIDDLE"
+                                    ,"WALK_LOCATE_SHOULDER", "WALK_SHOULDER_WAIST_1_10", "WALK_SHOULDER_WAIST_2_10", "WALK_SHOULDER_WAIST_3_10", "WALK_SHOULDER_WAIST_4_10"
+                                    ,"WALK_SHOULDER_WAIST_5_10", "WALK_SHOULDER_WAIST_6_10", "WALK_SHOULDER_WAIST_7_10", "WALK_SHOULDER_WAIST_8_10", "WALK_SHOULDER_WAIST_9_10"
+                                    ,"WALK_LOCATE_WAIST", "WALK_LOCATE_HIP_HIGH", "WALK_LOCATE_HIP_LOW"};
+#endif
+            string[] comboWalkLocation = { "PARK", "TOP", "HEAD_BACK", "NECK_POSITION", "NECK_MIDDLE"
+                                    ,"SHOULDER", "SHOULDER_WAIST_1_10", "SHOULDER_WAIST_2_10", "SHOULDER_WAIST_3_10", "SHOULDER_WAIST_4_10"
+                                    ,"SHOULDER_WAIST_5_10", "SHOULDER_WAIST_6_10", "SHOULDER_WAIST_7_10", "SHOULDER_WAIST_8_10", "HOULDER_WAIST_9_10"
+                                    ,"LOCATE_WAIST", "LOCATE_HIP_HIGH", "LOCATE_HIP_LOW"};
+            comboBox2.Items.AddRange(comboWalkLocation);
+
+            string[] comboWalkDuty = { "MOTOR_POWER_0", "MOTOR_POWER_1", "MOTOR_POWER_2", "MOTOR_POWER_3", "MOTOR_POWER_4", "MOTOR_POWER_5"};
+            comboBox3.Items.AddRange(comboWalkDuty);
+
+#if false
+            string[] comboWidth = { "STATE_KNEAD_IDLE", "STATE_KNEAD_CLOCK_RUN", "STATE_KNEAD_UNCLOCK_RUN", "STATE_KNEAD_STOP_AT_MIN", "STATE_KNEAD_STOP_AT_MID", "STATE_KNEAD_STOP_AT_MAX",
+                                    "STATE_KNEAD_UNSTOP_AT_MIN", "STATE_KNEAD_UNSTOP_AT_MID", "STATE_KNEAD_UNSTOP_AT_MAX", "STATE_KNEAD_SWING_MIN", "STATE_KNEAD_SWING_MID", "STATE_KNEAD_SWING_MAX"
+                                    "STATE_KNEAD_QS_MIN_CW", "STATE_KNEAD_QS_MIN_CCW"};
+#endif
+            string[] comboWidth = { "IDLE", "CLOCK_RUN", "UNCLOCK_RUN", "STOP_AT_MIN", "STOP_AT_MID", "STOP_AT_MAX",
+                                    "UNSTOP_AT_MIN", "UNSTOP_AT_MID", "UNSTOP_AT_MAX", "SWING_MIN", "SWING_MID", "SWING_MAX",
+                                    "QS_MIN_CW", "QS_MIN_CCW"};
+            comboBox4.Items.AddRange(comboWidth);
+
+            comboBox5.Items.AddRange(comboWalkDuty);
+            comboBox6.Items.AddRange(comboWalkDuty);
+
+            string[] comboXdHold = { "XD_STEP_0", "XD_STEP_1", "XD_STEP_2", "XD_STEP_3", "XD_STEP_4", "XD_STEP_5"};
+            comboBox7.Items.AddRange(comboXdHold);
+            comboBox8.Items.AddRange(comboWalkDuty);
+
+#if false
+            string[] comboXdRepeat = { "MASSAGE_XD_SECTION_0_1", "MASSAGE_XD_SECTION_0_2", "MASSAGE_XD_SECTION_0_3", "MASSAGE_XD_SECTION_0_4", "MASSAGE_XD_SECTION_0_5"
+                                        ,"MASSAGE_XD_SECTION_1_0", "MASSAGE_XD_SECTION_1_2", "MASSAGE_XD_SECTION_1_3", "MASSAGE_XD_SECTION_1_4", "MASSAGE_XD_SECTION_1_5"
+                                        ,"MASSAGE_XD_SECTION_2_0", "MASSAGE_XD_SECTION_2_1", "MASSAGE_XD_SECTION_2_3", "MASSAGE_XD_SECTION_2_4", "MASSAGE_XD_SECTION_2_5"
+                                        ,"MASSAGE_XD_SECTION_3_0", "MASSAGE_XD_SECTION_3_1", "MASSAGE_XD_SECTION_3_2", "MASSAGE_XD_SECTION_3_4", "MASSAGE_XD_SECTION_3_5"
+                                        ,"MASSAGE_XD_SECTION_4_0", "MASSAGE_XD_SECTION_4_1", "MASSAGE_XD_SECTION_4_2", "MASSAGE_XD_SECTION_4_3", "MASSAGE_XD_SECTION_4_5"
+                                        ,"MASSAGE_XD_SECTION_5_0", "MASSAGE_XD_SECTION_5_1", "MASSAGE_XD_SECTION_5_2", "MASSAGE_XD_SECTION_5_3", "MASSAGE_XD_SECTION_5_4"};
+#endif
+            string[] comboXdRepeat = { "SECTION_0_1", "SECTION_0_2", "SECTION_0_3", "SECTION_0_4", "SECTION_0_5"
+                                      ,"SECTION_1_0", "SECTION_1_2", "SECTION_1_3", "SECTION_1_4", "SECTION_1_5"
+                                      ,"SECTION_2_0", "SECTION_2_1", "SECTION_2_3", "SECTION_2_4", "SECTION_2_5"
+                                      ,"SECTION_3_0", "SECTION_3_1", "SECTION_3_2", "SECTION_3_4", "SECTION_3_5"
+                                      ,"SECTION_4_0", "SECTION_4_1", "SECTION_4_2", "SECTION_4_3", "SECTION_4_5"
+                                      ,"SECTION_5_0", "SECTION_5_1", "SECTION_5_2", "SECTION_5_3", "SECTION_5_4"};
+
+            comboBox9.Items.AddRange(comboXdRepeat);
+
+            string[] comboInterWork = { "INTERWORK_AIR_HIP", "INTERWORK_AIR_SHOULDER", "INTERWORK_AIR_HIP_RELEASE", "INTERWORK_AIR_SEAT_BACK", "INTERWORK_AIR_SEAT_NOT_SHOULDER"
+                                      ,"INTERWORK_LEG_DOWN", "INTERWORK_BRAIN_ALL_ARM", "INTERWORK_BRAIN_RIGHT_ARM", "INTERWORK_BRAIN_LEFT_ARM", "INTERWORK_BRAIN_SHOULDER_FOOT_HEEL"
+                                      ,"INTERWORK_BRAIN_FOOT_HEEL_LEG_SHOULDER", "INTERWORK_BRAIN_WAIST_SHOULDER_6S", "INTERWORK_BRAIN_WAIST_SHOULDER_5S", "INTERWORK_BRAIN_ALL_DISABLE"};
+
+            comboBox10.Items.AddRange(comboInterWork);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex >= 0)
+            {
+                comboBox1.Text = comboBox1.SelectedIndex.ToString();
+            }
+        }
+
+        private void drawRichTextBod()
+        {
+            richTextBox1.Clear();
+
+            for (int listnum = 0; listnum < massageList.Count; listnum++)
+            {
+                richTextBox1.Text += massageList[listnum].massageType + String.Empty.PadRight(4, ' ');
+                richTextBox1.Text += massageList[listnum].walkHoldTime + String.Empty.PadRight(4, ' ');
+                richTextBox1.Text += massageList[listnum].walkSpeed + String.Empty.PadRight(4, ' ');
+                richTextBox1.Text += massageList[listnum].width + String.Empty.PadRight(4, ' ');
+                richTextBox1.Text += massageList[listnum].tDuty + String.Empty.PadRight(4, ' ');
+                richTextBox1.Text += massageList[listnum].kDuty + String.Empty.PadRight(4, ' ');
+                richTextBox1.Text += massageList[listnum].xdHoldTime + String.Empty.PadRight(4, ' ');
+                richTextBox1.Text += massageList[listnum].xdRepeat + String.Empty.PadRight(4, ' ');
+                richTextBox1.Text += massageList[listnum].xdAdj + String.Empty.PadRight(4, ' ');
+                richTextBox1.Text += massageList[listnum].interwork + String.Empty.PadRight(4, ' ');
+
+                richTextBox1.Text += Environment.NewLine;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            massageList.Add(new MassageMap(comboBox1.Text.ToString(), comboBox2.Text.ToString(), comboBox3.Text.ToString(), comboBox4.Text.ToString(), comboBox5.Text.ToString(),
+                comboBox6.Text.ToString(), comboBox7.Text.ToString(), comboBox8.Text.ToString(), comboBox9.Text.ToString(), "XD_AD_STEP_MAX", comboBox10.Text.ToString()));
+
+            drawRichTextBod();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (massageList.Count == 0)
+            {
+                return;
+            }
+
+            massageList.RemoveAt(massageList.Count - 1);
+
+            drawRichTextBod();
+        }
+    }
+}
